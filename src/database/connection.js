99114bb74a9d5e124b60445d37b552ad3e6dc582
mauro guardarini <mauro.guardarini@gmail.com>
2017-11-20 13:44:57 -0200

    change the connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export function ConnectDatabase(){
 
-    const promise = mongoose.connect('mongodb://analytics:123456@ds159845.mlab.com:59845/analytics', {
+    const promise = mongoose.connect('mongodb://<your database>', {
       useMongoClient: true
     });
 
