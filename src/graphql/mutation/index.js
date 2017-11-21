c44a87d48099aa3aac6880dc3b5ee8070ba2da9f
Mauro Guardarini <mauro.guardarini@gmail.com>
2017-11-21 10:20:35 -0200

    update index.js

--- src/graphql/mutation/index.js
+++ src/graphql/mutation/index.js
@@ -1,13 +1,3 @@
-// import analyticsMutation from "./analytics";
-//
-// export default {
-//   analyticsMutation
-// }
-// export default {
-//   ...userMutation,
-//   ...postMutation
-// }
-
 import analyticsMutation from './analytics';
 
 export default analyticsMutation;
